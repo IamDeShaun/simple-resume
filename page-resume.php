52e1dc89a5d9d6c09b30044a07dbcde6341b34ed
DeShaun Jones <viceprezofnv@gmail.com>
2018-11-05 16:32:01 -0500

    Resume Page
    Created custom page templete for Resume. Embed resume using google drive.

---
+++ page-resume.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Resume Page */
+
+get_header(); ?>
+
+<!-- Resume Section -->
+<section id="resume">
+  
+  <iframe src="https://drive.google.com/file/d/177KospaEkOc8gKUkuTfpfamFINgT1ati/preview" width="100%" height="1200"></iframe>
+
+
+</section>
+
+<?php get_footer(); ?>
